6f2ec0a7bae9717a119cb7acafcee1e192480516
vgoryachev <vgoryachev@at-consulting.ru>
2015-12-07 11:02:09 +0300

    add host and status for error messages

--- src/main/java/ru/atc/camel/nnm/devices/WsdlNNMConsumer.java
+++ src/main/java/ru/atc/camel/nnm/devices/WsdlNNMConsumer.java
@@ -222,6 +222,8 @@ public class WsdlNNMConsumer extends ScheduledPollConsumer {
 		genevent.setSeverity(PersistentEventSeverity.CRITICAL.name());
 		genevent.setTimestamp(timestamp);
 		genevent.setEventsource("NNM_DEVICE_ADAPTER");
+		genevent.setStatus("OPEN");
+		genevent.setHost("adapter");
 		
 		logger.info(" **** Create Exchange for Error Message container");
         Exchange exchange = getEndpoint().createExchange();
